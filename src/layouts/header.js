56c24d1dce7b8cf711c4bbca5cc2267e4265db60
Guru Ramprakash <gururamp30@gmail.com>
2018-06-05 18:52:08 +0530

    New Lear Logo.

--- src/layouts/header.js
+++ src/layouts/header.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import '../css/header.css';
 import logo from '../images/logo.png';
+import LearLogo from '../images/lear-logo.png';
+
 import {ActionButtons} from '../components/action-buttons';
 
 
@@ -11,11 +13,18 @@ export class Header extends Component {
             <div className="header-part">
               <div className="page-logo">
                 <figure className="logo">
-                  <img src={logo} alt="Carma Networks" title="Carma Networks" />
+                  <div className="row" id="banner">
+                    <div className="column">
+                      <img src={logo} alt="Carma Networks" title="Carma Networks" />
+                    </div>
+                    <div className="column">
+                      <img src={LearLogo} alt="Lear Corporation" title="Lear" />
+                    </div>
+                </div>
                 </figure>
               </div>
               <div className="header-title">Transit Signal Priority</div>
-          <ActionButtons srmenable={this.props.srmenable} ssmenable={this.props.ssmenable} clearData={this.props.clearData} 
+          <ActionButtons srmenable={this.props.srmenable} ssmenable={this.props.ssmenable} clearData={this.props.clearData}
           toggleNotifications={this.props.toggleNotifications} toggleBus={this.props.toggleBus}/>
             </div>
         </header>
