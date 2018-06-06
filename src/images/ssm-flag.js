53d65f26ca3d08facdb73799be5ca724c0d357ed
Guru Ramprakash <gururamp30@gmail.com>
2018-06-06 17:34:07 +0530

    SSM Flag with count.

--- src/images/ssm-flag.js
+++ src/images/ssm-flag.js
@@ -1,5 +1,5 @@
 const ssmIcon = [
-    '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="178px" height="216px" viewBox="0 0 178 216" transform=""  version="1.1">',
+    '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="178px" height="216px" viewBox="0 0 356 216" transform=""  version="1.1">',
     '<g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">',
         '<g id="flag-op-2">',
             '<path d="M20.9,215.9 L20.9,10.057 C20.899,4.503 16.396,0 10.841,0 C5.286,0 0.784,4.503 0.784,10.057 L0.784,30.681 L0.784,113.178 L0.784,215.9 L20.9,215.9 Z" id="Shape" fill="fillColor" fill-rule="nonzero"></path>',
@@ -7,6 +7,8 @@ const ssmIcon = [
             '<text id="SSM" font-family="Arial-BoldMT, Arial" font-size="55" font-weight="bold" fill="#FFFFFF">',
                 '<tspan x="34" y="90">SSM</tspan>',
             '</text>',
+            '<ellipse xmlns="http://www.w3.org/2000/svg" id="Oval" fill="#0000FF" cx="185.5" cy="26" rx="29.5" ry="30"/>',
+            'count',
         '</g>',
     '</g>',
     '</svg>'].join('\n');
